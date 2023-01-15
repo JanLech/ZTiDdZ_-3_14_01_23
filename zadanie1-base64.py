09e857edc61e142cd4a71d18da6c3fb2a56f5d55
JanLech <90306836+JanLech@users.noreply.github.com>
2023-01-15 10:38:16 +0100

    Delete zadanie1-base64.py

--- zadanie1-base64.py
+++
@@ -1,8 +0,0 @@
-import base64
-def encrypt_base64(text):
-    text_bytes = text.encode('utf-8')
-    base64_bytes = base64.b64encode(text_bytes)
-    base64_message = base64_bytes.decode('utf-8')
-    return base64_message
-# test funkcji
-print(encrypt_base64("Hello world"))
